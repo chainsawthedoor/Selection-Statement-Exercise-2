b3dea450014f2270710e9fa108faeabef3c64f65
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-02-18 17:09:05 -0500

    Created method FavSubject, added lines 10-36 to code. Also, commiting/pushing with ctrl+k in Rider!

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What's your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I'm not so good with Math, personally!");
+                    break;
+                case "science":
+                    Console.WriteLine("Love science!");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical education":    
+                    Console.WriteLine("P.E. is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("I love history too!");
+                    break;
+                case "geography":
+                    Console.WriteLine("What a nerd!");
+                    break;
+                default:
+                    Console.WriteLine("Hey, I like that too!");
+                    break;
+            }
         }
     }
 }
